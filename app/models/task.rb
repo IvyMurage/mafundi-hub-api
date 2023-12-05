8ecab75a9d55b1a97d1b146b737be6eb5512da6c
Murage-Ivy <ivymurage2000@gmail.com>
2023-12-05 13:16:54 +0300

    Add task labels

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,12 @@
 class Task < ApplicationRecord
   belongs_to :client
+  belongs_to :service
+
+  has_one :location, as: :locationable, dependent: :destroy
+  accepts_nested_attributes_for :location
+
+  validates task_description, presence: true
+  validates job_price, presence: true
+  validates job_title, presence: true
+  validates location, presence: true
 end
