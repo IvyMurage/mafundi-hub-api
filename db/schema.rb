b0db2d6320c74686bdf216bda499355397397841
Murage-Ivy <ivymurage2000@gmail.com>
2023-12-20 14:24:17 +0300

    Remove payment method column from appointments table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_20_111441) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_20_112143) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -53,6 +53,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_20_111441) do
     t.time "appointment_time"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "payment_method"
     t.index ["client_id"], name: "index_appointments_on_client_id"
     t.index ["handyman_id"], name: "index_appointments_on_handyman_id"
     t.index ["task_id"], name: "index_appointments_on_task_id"
