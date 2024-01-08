3daecbc7ed51d52b3c13b27930a42fd9e1e610a0
Murage-Ivy <ivymurage2000@gmail.com>
2024-01-08 23:47:03 +0300

    Commit message:
    ```
    Updated the script to use 'bundle exec' for 'rails assets:precompile', 'rails assets:clean', and 'rails db:migrate' commands.
    ```

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,7 +1,10 @@
-#!/usr/bin/env bash
 # exit on error
 set -o errexit
 
 bundle install
-./bin/rails assets:precompile
-./bin/rails assets:clean
+bundle exec rails assets:precompile
+bundle exec rails assets:clean
+bundle exec rails db:migrate
+
+#if you have seeds to run add:
+# bundle exec rails db:seed
