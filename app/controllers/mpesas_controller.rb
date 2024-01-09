68ec18021251ba437a1ab8f9394619612b84b782
Murage-Ivy <ivymurage2000@gmail.com>
2024-01-09 11:44:34 +0300

    Changed Base64 encoding method from strict_encode64 to strict_encode64.

--- app/controllers/mpesas_controller.rb
+++ app/controllers/mpesas_controller.rb
@@ -98,7 +98,7 @@ class MpesasController < ApplicationController
     @url = "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials"
     @consumer_key = ENV["MPESA_CONSUMER_KEY"]
     @consumer_secret = ENV["MPESA_CONSUMER_SECRET"]
-    @userpass = Base64::strict_encode64("#{@consumer_key}:#{@consumer_secret}")
+    @userpass = Base64.strict_encode64("#{@consumer_key}:#{@consumer_secret}")
     @headers = {
       Authorization: "Bearer #{@userpass}",
     }
