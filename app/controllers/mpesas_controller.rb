65a87dd7e429c476f4a2b5e843a9717860238e76
Murage-Ivy <ivymurage2000@gmail.com>
2024-01-09 15:12:27 +0300

    Update error handling for M-Pesa API responses

--- app/controllers/mpesas_controller.rb
+++ app/controllers/mpesas_controller.rb
@@ -42,7 +42,7 @@ class MpesasController < ApplicationController
       when 200
         [:success, JSON.parse(response.to_str)]
       when 400
-        render json: [:error, JSON.parse(response.to_str)]
+        [:error, JSON.parse(response.to_str)]
       else
         fail "Invalid response #{response.to_str} received"
       end
@@ -83,7 +83,7 @@ class MpesasController < ApplicationController
       when 200
         [:success, JSON.parse(response.to_str)]
       when 400
-        render json: { error: JSON.parse(response.to_str) }
+        [error: JSON.parse(response.to_str)]
       else
         fail "Invalid response #{response.to_str} received"
       end
