13b3eb069acd37edee21e4edfcd7e85c9a196a82
Murage-Ivy <ivymurage2000@gmail.com>
2024-01-09 10:05:45 +0300

    Comment out assets:precompile and assets:clean

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,9 +2,9 @@
 set -o errexit
 
 bundle install
-bundle exec rails assets:precompile
-bundle exec rails assets:clean
+# bundle exec rails assets:precompile
+# bundle exec rails assets:clean
 bundle exec rails db:migrate
 
 #if you have seeds to run add:
-# bundle exec rails db:seed
+bundle exec rails db:seed
