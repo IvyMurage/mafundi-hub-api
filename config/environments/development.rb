c8dce901ffd2de5b2c7446e208bfe2d462876764
Murage-Ivy <ivymurage2000@gmail.com>
2024-01-09 11:17:15 +0300

    Update config/environments/development.rb to add https://mafundi-hub-api.onrender.com to config.hosts

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -7,7 +7,7 @@ Rails.application.configure do
   # it changes. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
 
-  config.hosts >> "https://mafundi-hub-api.onrender.com"
+  config.hosts << "https://mafundi-hub-api.onrender.com"
 
   config.hosts << /[a-z0-9\-]+\.ngrok-free\.app/
 
