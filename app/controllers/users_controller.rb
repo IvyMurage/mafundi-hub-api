49ed408139ef5cb5228581ec542bd8f63a9dc269
Murage-Ivy <ivymurage2000@gmail.com>
2024-01-10 09:43:07 +0300

    Add user CRUD actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,12 +19,44 @@ class UsersController < ApplicationController
 
   def index
     @users = User.page(params[:page]).per(params[:per_page] || 10)
-    users_json = ActiveModelSerializers::SerializableResource.new(@users, each_serializer: MafundiUserSerializer).as_json
     render json: { meta: pagination_meta(@users), user: users_json }, status: :ok
   end
 
   def show
-    @user = User.find(params[:id])
+    @user = set_user
     render json: @user, serializer: MafundiUserSerializer, status: :ok
+  end
+
+  def create
+    @user = User.create!(user_params)
+    render json: @user, serializer: MafundiUserSerializer, status: :created
+  end
+
+  def update
+    @user = set_user
+    @user.update!(user_params)
+    render json: @user, serializer: MafundiUserSerializer, status: :ok
+  end
+
+  def destroy
+    @user = set_user
+    @user.destroy
+    head :no_content
+  end
+
+  private
+
+  def users_json
+    ActiveModelSerializers::SerializableResource.new(@users, each_serializer: MafundiUserSerializer).as_json
+  end
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  # Only allow a trusted parameter "white list" through.
+  def user_params
+    params.permit(:email, :password, :password_confirmation)
   end
 end
