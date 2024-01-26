47127c326a3483561d9d36c005f21b392fac130d
Murage-Ivy <ivymurage2000@gmail.com>
2024-01-26 11:53:31 +0300

    Commit message:
    ```
    Comment out asset precompilation and cleanup commands.
    ```

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,8 +2,8 @@
 set -o errexit
 
 bundle install
-bundle exec rails assets:precompile
-bundle exec rails assets:clean
+# bundle exec rails assets:precompile
+# bundle exec rails assets:clean
 bundle exec rails db:migrate
 
 #if you have seeds to run add:
