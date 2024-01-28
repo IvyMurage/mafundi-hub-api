23735f5e720d8735a9498dc07ea6b2761bfc2e46
Murage-Ivy <ivymurage2000@gmail.com>
2024-01-28 20:23:51 +0300

    User can only be created once

--- app/models/client.rb
+++ app/models/client.rb
@@ -7,4 +7,10 @@ class Client < ApplicationRecord
   has_many :handymen, through: :appointments
 
   accepts_nested_attributes_for :location
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :phone_number, presence: true
+  # validates :user_id, presence: true
+  validates :user_id, uniqueness: { scope: :user_id, message: "Already exists." } # Task description must be unique per client.
 end
