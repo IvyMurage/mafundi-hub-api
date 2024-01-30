6b1cb75a234aa373eb4f7453b1d700a2692266a5
Murage-Ivy <ivymurage2000@gmail.com>
2024-01-30 22:14:30 +0300

    Add client id to user

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,13 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :email
   has_many :roles
+
+  def user_role
+    object.roles.first.name
+  end
+
+  def user_id
+    object.client.id if user_role == "client"
+    objcet.handyman.id if user_role == "handyman"
+  end
 end
