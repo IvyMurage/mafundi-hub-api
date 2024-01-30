32535dced4e57449d8829ea600d35639e2fa3e8c
Murage-Ivy <ivymurage2000@gmail.com>
2024-01-30 22:30:31 +0300

    Resolve bug

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,13 +1,12 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :email, :user_id, :user_role
-  has_many :roles
 
   def user_role
     object.roles.first.name
   end
 
   def user_id
-    object.client.id if user_role == "client"
-    objcet.handyman.id if user_role == "handyman"
+    object.client.id if user_role === "client"
+    object.handyman.id if user_role === "handyman"
   end
 end
