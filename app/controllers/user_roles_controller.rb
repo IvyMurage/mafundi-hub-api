d9c1b79634584cee537768ce317ad7003f8cbb2a
Murage-Ivy <ivymurage2000@gmail.com>
2024-01-31 14:21:58 +0300

    Change role serializer

--- app/controllers/user_roles_controller.rb
+++ app/controllers/user_roles_controller.rb
@@ -16,7 +16,7 @@ class UserRolesController < ApplicationController
       return
     end
 
-    render json: { user: UserSerializer.new(user), status: "User role updated successfully" }
+    render json: { user: UserSignupSerializer.new(user), status: "User role updated successfully" }
   end
 
   private
