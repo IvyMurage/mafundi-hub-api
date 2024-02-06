933b1e45a117681f97f9fd86910eb4b12bf9e607
Murage-Ivy <ivymurage2000@gmail.com>
2024-02-06 17:28:45 +0300

    unable permisiions for tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
   before_action :authenticate_user!
-  # load_and_authorize_resource
+  load_and_authorize_resource
   include Pagination
 
   rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
