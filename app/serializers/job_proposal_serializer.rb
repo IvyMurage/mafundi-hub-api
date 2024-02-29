c444fd9b13fbe18a0a9d0da8dfaea777323d21e2
Murage-Ivy <ivymurage2000@gmail.com>
2024-02-29 12:54:50 +0300

    Return handyman id

--- app/serializers/job_proposal_serializer.rb
+++ app/serializers/job_proposal_serializer.rb
@@ -1,8 +1,11 @@
 class JobProposalSerializer < ActiveModel::Serializer
-  attributes :id, :job_status, :proposal_text, :task_title
-  has_one :handyman
+  attributes :id, :job_status, :proposal_text, :task_title, handyman_id
 
   def task_title
     object.task.job_title
+  end
+
+  def handyman_id
+    object.handyman.id
   end
 end
