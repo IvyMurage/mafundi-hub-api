67c9d2387ba99acb3b6dc9155a5c10e6edc2a5e5
Murage-Ivy <ivymurage2000@gmail.com>
2024-02-29 10:59:54 +0300

    Get job proposals using handyman id

--- app/controllers/job_proposals_controller.rb
+++ app/controllers/job_proposals_controller.rb
@@ -19,6 +19,7 @@ class JobProposalsController < ApplicationController
   def index
     @job_proposals = JobProposal.page(params[:page]).per(params[:per_page] || 10)
     @job_proposals = @job_proposals.where(task_id: job_proposal_params[:task_id]) if params[:task_id].present?
+    @job_proposals = @job_proposals.where(handyman_id: job_proposal_params[:handyman_id]) if params[:handyman_id].present?
     @job_proposals = @job_proposals.by_location(params[:city]) if params[:city].present?
     @job_proposals = @job_proposal.by_service(params[:service_id]) if params[:service_id].present?
     job_proposals_json = ActiveModelSerializers::SerializableResource.new(@job_proposals, each_serializer: JobProposalSerializer).as_json
