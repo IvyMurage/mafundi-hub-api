83035eb81e69049a8df8e1e310676fa4e9297c5d
Murage-Ivy <ivymurage2000@gmail.com>
2024-02-29 11:05:05 +0300

    Render task title on job proposals

--- app/serializers/job_proposal_serializer.rb
+++ app/serializers/job_proposal_serializer.rb
@@ -1,4 +1,8 @@
 class JobProposalSerializer < ActiveModel::Serializer
-  attributes :id, :job_status, :proposal_text
+  attributes :id, :job_status, :proposal_text, :task_title
   has_one :handyman
+
+  def task_title
+    object.task.job_title
+  end
 end
