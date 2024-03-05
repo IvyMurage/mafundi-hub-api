2eb387d38d7dcdb12624fa5a0c6564fc51957bbf
Murage-Ivy <ivymurage2000@gmail.com>
2024-03-05 11:42:03 +0300

    log res again

--- app/controllers/mpesas_controller.rb
+++ app/controllers/mpesas_controller.rb
@@ -77,6 +77,7 @@ class MpesasController < ApplicationController
 
   def get_access_token
     res = generate_access_token
+    puts "res", res.inspect
 
     unless res.is_a?(Net::HTTPSuccess)
       raise MpesaError, "Unable to generate access token"
