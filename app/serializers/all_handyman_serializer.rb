268c7b5c489c8149eeb9543e8ab11c88f20866e7
Murage-Ivy <ivymurage2000@gmail.com>
2024-03-05 20:27:09 +0300

    Render handyman avatar

--- app/serializers/all_handyman_serializer.rb
+++ app/serializers/all_handyman_serializer.rb
@@ -1,9 +1,13 @@
 class AllHandymanSerializer < ActiveModel::Serializer
-  attributes :id, :first_name, :last_name, :title, :year_of_experience, :availability, :user_rating
+  attributes :id, :first_name, :last_name, :title, :year_of_experience, :availability, :user_rating, :avatar_url
 
   has_one :location, as: :locationable, serializer: LocationSerializer
 
   def user_rating
     object.reviews.average(:rating).to_f
+  end
+
+  def avatar_url
+    object.user.avatar.url
   end
 end
